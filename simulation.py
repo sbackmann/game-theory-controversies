4247b003235b8b4012340ec2887e1351e60379c2
sbackmann <sbackmann@student.ethz.ch>
2023-06-13 11:10:47 +0200

    Added deliberate infeciton possibility

--- simulation.py
+++ simulation.py
@@ -5,10 +5,12 @@ import matplotlib.pyplot as plt
 
 # Define the population
 population_size = 10000
-population = pd.DataFrame({'state': ['susceptible'] * population_size})
+population = pd.DataFrame({'state': ['susceptible'] * population_size,
+                           'infection_type': [''] * population_size})
 
 # Define the parameters
 infection_probability = [np.random.normal(loc=np.sin(i/10)/50, scale=0.01) for i in range(100)]
+infection_willingness = 0.005  # Probability of deliberately infecting oneself
 recovery_probability_infected = 0.05  # Recovery probability for infected individuals
 recovery_probability_severe = 0.05  # Recovery probability for severely infected individuals
 recovery_probability_hospitalized = 0.2  # Recovery probability for hospitalized individuals
@@ -20,11 +22,17 @@ num_time_steps = len(infection_probability)  # Number of time steps to simulate
 
 # Track statistics
 susceptible_count = []
-infected_count = []
-severely_infected_count = []
-hospitalized_count = []
-dead_count = []
-recovered_count = []
+infected_count_chance = []
+infected_count_deliberate = []
+severely_infected_count_chance = []
+severely_infected_count_deliberate = []
+hospitalized_count_chance = []
+hospitalized_count_deliberate = []
+dead_count_chance = []
+dead_count_deliberate = []
+recovered_count_chance = []
+recovered_count_deliberate = []
+
 
 # Simulation loop
 for i in range(num_time_steps):
@@ -33,11 +41,16 @@ for i in range(num_time_steps):
 
     # Track the counts of each state
     susceptible_count.append(state_counts.get('susceptible', 0))
-    infected_count.append(state_counts.get('infected', 0))
-    severely_infected_count.append(state_counts.get('severely infected', 0))
-    hospitalized_count.append(state_counts.get('hospitalized', 0))
-    dead_count.append(state_counts.get('dead', 0))
-    recovered_count.append(state_counts.get('recovered', 0))
+    infected_count_chance.append(np.sum((population['state'] == 'infected') & (population['infection_type'] == 'chance')))
+    infected_count_deliberate.append(np.sum((population['state'] == 'infected') & (population['infection_type'] == 'deliberate')))
+    severely_infected_count_chance.append(np.sum((population['state'] == 'severely infected') & (population['infection_type'] == 'chance')))
+    severely_infected_count_deliberate.append(np.sum((population['state'] == 'severely infected') & (population['infection_type'] == 'deliberate')))
+    hospitalized_count_chance.append(np.sum((population['state'] == 'hospitalized') & (population['infection_type'] == 'chance')))
+    hospitalized_count_deliberate.append(np.sum((population['state'] == 'hospitalized') & (population['infection_type'] == 'deliberate')))
+    dead_count_chance.append(np.sum((population['state'] == 'dead') & (population['infection_type'] == 'chance')))
+    dead_count_deliberate.append(np.sum((population['state'] == 'dead') & (population['infection_type'] == 'deliberate')))
+    recovered_count_chance.append(np.sum((population['state'] == 'recovered') & (population['infection_type'] == 'chance')))
+    recovered_count_deliberate.append(np.sum((population['state'] == 'recovered') & (population['infection_type'] == 'deliberate')))
 
     # Generate random numbers for state transitions
     random_numbers = np.random.random(population_size)
@@ -46,7 +59,17 @@ for i in range(num_time_steps):
     susceptible_mask = (population['state'] == 'susceptible')
     infected_mask = (random_numbers < infection_probability[i])
     population.loc[susceptible_mask & infected_mask, 'state'] = 'infected'
+    population.loc[susceptible_mask & infected_mask, 'infection_type'] = 'chance'
     random_numbers = np.random.random(population_size)
+
+    # Check if individuals decide to get deliberately infected
+    low_hospital_utilization = ((hospitalized_count_chance[-1] + hospitalized_count_deliberate[-1]) / hospital_capacity) < 0.5  # Define the threshold for low utilization
+    susceptible_mask = (population['state'] == 'susceptible')
+    random_numbers = np.random.random(population_size)
+    deliberate_infection_mask = (random_numbers < infection_willingness) & low_hospital_utilization
+    population.loc[susceptible_mask & deliberate_infection_mask, 'state'] = 'infected'
+    population.loc[susceptible_mask & deliberate_infection_mask, 'infection_type'] = 'deliberate'
+
     
     # Check if infected individuals transition to severely infected
     infected_mask = (population['state'] == 'infected')
@@ -79,15 +102,55 @@ for i in range(num_time_steps):
     recovery_mask = (random_numbers < recovery_probability_infected)
     population.loc[infected_mask & recovery_mask, 'state'] = 'recovered'
 
+
+
 # Plot the epidemic progression
 time_steps = range(num_time_steps)
-plt.semilogy(time_steps, susceptible_count, label='Susceptible')
-plt.semilogy(time_steps, infected_count, label='Infected')
-plt.semilogy(time_steps, severely_infected_count, label='Severely Infected')
-plt.semilogy(time_steps, hospitalized_count, label='Hospitalized')
-plt.semilogy(time_steps, dead_count, label='Dead')
-plt.semilogy(time_steps, recovered_count, label='Recovered')
-plt.xlabel('Time Steps')
-plt.ylabel('Population Count (Log Scale)')
-plt.legend()
+
+# Define line styles and markers for each infection type
+line_styles = {'chance': '-', 'deliberate': '--'}
+markers = {'chance': 'o', 'deliberate': 's'}
+
+# Create a figure with two subplots
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 6))
+
+# Plot for infection type: chance
+axes[0].semilogy(time_steps, susceptible_count, label='Susceptible', linestyle=line_styles['chance'], marker=markers['chance'])
+axes[0].semilogy(time_steps, infected_count_chance, label='Infected (Chance)', linestyle=line_styles['chance'], marker=markers['chance'])
+axes[0].semilogy(time_steps, severely_infected_count_chance, label='Severely Infected (Chance)', linestyle=line_styles['chance'], marker=markers['chance'])
+axes[0].semilogy(time_steps, hospitalized_count_chance, label='Hospitalized (Chance)', linestyle=line_styles['chance'], marker=markers['chance'])
+axes[0].semilogy(time_steps, dead_count_chance, label='Dead (Chance)', linestyle=line_styles['chance'], marker=markers['chance'])
+axes[0].semilogy(time_steps, recovered_count_chance, label='Recovered (Chance)', linestyle=line_styles['chance'], marker=markers['chance'])
+axes[0].set_xlabel('Time Steps')
+axes[0].set_ylabel('Population Count (Log Scale)')
+axes[0].set_title('Epidemic Progression (Infection Type: Chance)')
+axes[0].legend()
+axes[0].set_yscale('log')
+
+# Add infection type annotations for chance
+state_counts_chance = population[population['infection_type'] == 'chance']['state'].value_counts()
+for state, count in state_counts_chance.items():
+    if state != 'susceptible':
+        axes[0].annotate(f"{state} ({count})", xy=(num_time_steps - 1, count), xytext=(5, 0), textcoords='offset points')
+
+# Plot for infection type: deliberate
+axes[1].semilogy(time_steps, susceptible_count, label='Susceptible', linestyle=line_styles['deliberate'], marker=markers['deliberate'])
+axes[1].semilogy(time_steps, infected_count_deliberate, label='Infected (Deliberate)', linestyle=line_styles['deliberate'], marker=markers['deliberate'])
+axes[1].semilogy(time_steps, severely_infected_count_deliberate, label='Severely Infected (Deliberate)', linestyle=line_styles['deliberate'], marker=markers['deliberate'])
+axes[1].semilogy(time_steps, hospitalized_count_deliberate, label='Hospitalized (Deliberate)', linestyle=line_styles['deliberate'], marker=markers['deliberate'])
+axes[1].semilogy(time_steps, dead_count_deliberate, label='Dead (Deliberate)', linestyle=line_styles['deliberate'], marker=markers['deliberate'])
+axes[1].semilogy(time_steps, recovered_count_deliberate, label='Recovered (Deliberate)', linestyle=line_styles['deliberate'], marker=markers['deliberate'])
+axes[1].set_xlabel('Time Steps')
+axes[1].set_ylabel('Population Count (Log Scale)')
+axes[1].set_title('Epidemic Progression (Infection Type: Deliberate)')
+axes[1].legend()
+axes[1].set_yscale('log')
+
+# Add infection type annotations for deliberate
+state_counts_deliberate = population[population['infection_type'] == 'deliberate']['state'].value_counts()
+for state, count in state_counts_deliberate.items():
+    if state != 'susceptible':
+        axes[1].annotate(f"{state} ({count})", xy=(num_time_steps - 1, count), xytext=(5, 0), textcoords='offset points')
+
+plt.tight_layout()
 plt.show()
